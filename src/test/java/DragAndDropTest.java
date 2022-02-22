dd094f9de70aebb75475a819b71e5218d0cfc87e
Igor Zvyagintsev <zvyagintsevis@yandex.ru>
2022-02-22 23:56:33 +0300

    Add homework №5

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,15 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+    @Test
+    void dragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").should(Condition.text("B"));
+        $("#column-b").should(Condition.text("A"));
+    }
+}
---
+++ src/test/java/GitHubTest.java
@@ -0,0 +1,18 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitHubTest {
+    @Test
+    void gitHubTest() {
+        open("https://github.com/selenide");
+        $(byTitle("selenide")).click();
+        $("#wiki-tab").click();
+        $(byText("Soft assertions")).should(Condition.exist).click();
+        $(".application-main ").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
